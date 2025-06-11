778b909ec931dc40f915a7f2fbc038a9b726ea1d
Student <khemrajbhagat236@gmail.com>
2025-06-11 11:08:49 +0530

    Day2

---
+++ Class basics.py
@@ -0,0 +1,14 @@
+class Apple_design:
+    count=0 #class variables or static variables
+    def __init__(self,cpu,ram):  #Constructor
+        self.cpu=cpu
+        self.ram=ram
+    def mobile(self,cpu,ram):
+        print("this apple phone 1")
+        print(self.cpu,self.ram)
+        print(type(self))
+
+m1=Apple_design(3.5,8)
+m2=Apple_design(4.5,16)
+m1.mobile(3.5,8)
+m2.mobile(4.5,16)
